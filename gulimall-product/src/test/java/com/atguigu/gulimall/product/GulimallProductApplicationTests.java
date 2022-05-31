319ce88f304e0c47afbb6d01820f3166e0ad77f1
yalongZhu <1032065185@qq.com>
2022-05-31 23:34:08 +0800

    P63(未完成)

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,8 @@
 package com.atguigu.gulimall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -7,10 +10,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
+
+    @Autowired
+    OSSClient ossClient;
 
     @Autowired
     BrandService brandService;
@@ -33,4 +42,32 @@ class GulimallProductApplicationTests {
 
     }
 
+    @Test
+    void testUpload() throws FileNotFoundException {
+//        String endpoint = "oss-cn-shenzhen.aliyuncs.com";
+//        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+//        String accessKeyId = "LTAI5tJcwnhokAPdHR1VLdka";
+//        String accessKeySecret = "UkET5z4z6uDI8cWklaamjNhF9FnSiv";
+//
+//        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "gulimall-yalong";
+//        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "oppo.png";
+//        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+//        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        String filePath= "E:\\谷粒mall\\docs\\pics\\oppo.png";
+//
+//        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+//
+        InputStream inputStream = new FileInputStream(filePath);
+//        // 创建PutObject请求。
+        ossClient.putObject(bucketName, objectName, inputStream);
+        ossClient.shutdown();
+
+        System.out.println("success");
+    }
 }
+
+
+
